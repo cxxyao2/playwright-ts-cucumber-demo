b12ee5bcc4916d4cb659ee144af0aa789ef4f5b8
jennifer <cxxyao2@gmail.com>
2026-04-19 22:20:04 -0400

    feat: add cucumber reporting and hooks for Playwright tests

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -1,20 +1,45 @@
-import { BeforeAll, AfterAll } from '@cucumber/cucumber';
-import { Browser, chromium, Page } from '@playwright/test';
+import { Before, After, AfterAll, Status, AfterStep } from '@cucumber/cucumber';
+import { Browser, BrowserContext, chromium, Page } from '@playwright/test';
 import { pageFixture } from './pageFixture';
 
 let browser: Browser;
-let page: Page;
+let context: BrowserContext;
 
-BeforeAll(async function () {
+Before(async function () {
     browser = await chromium.launch({ headless: false });
-    page = await browser.newPage();
+
+});
+
+
+Before(async function () {
+    context = await browser.newContext();
+    const page = await browser.newPage();
     pageFixture.page = page;
 
 });
 
-AfterAll(async function () {
-    await pageFixture.page.close();
-    await browser.close();
+// AfterStep(async function ({ pickle, result }) {
+//     const img = await pageFixture.page.screenshot({ path: `./screenshots/${pickle.name}` + `-${new Date().getTime()}.png` });
+//     await this.attach(img, 'image/png');
+// });
 
-    console.log('AfterAll');
+
+After(async function ({ pickle, result }) {
+    if (result?.status === Status.FAILED) {
+        console.log(`Scenario Failed: ${pickle.name}`);
+        // screenshot, pickle.name = scenario.name
+        const img = await pageFixture.page.screenshot({ path: `./screenshots/${pickle.name}` + `-${new Date().getTime()}.png` });
+        await this.attach(img, 'image/png');
+    }
+    else {
+        console.log(`Scenario Passed: ${pickle.name}`);
+    }
+
+
+    await pageFixture.page.close();
+    await context.close();
+});
+
+AfterAll(async function () {
+    await browser.close();
 });
